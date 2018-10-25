bb6192fdadeaa9c3baf3cf2e7b5fc761561a3f53
zohar bouchnik <zohar.bouchnik@mail.huji.ac.il>
2018-10-25 20:01:46 +0300

    fill is with bug

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,61 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@color
+M=0 // init color
+
+@8191
+D=A
+@iter
+M=D  // the number of iterations in the loop (flaten screen size)
+
+(INPUTLOOP)
+
+@i
+M=0  // init iterations counter
+
+@SCREEN
+D=A
+@address
+M=D  // init the address to the screen address
+
+@KBD
+D=M  //get input
+
+@WHITE
+D;JEQ // no input from the user
+
+(BLACK)
+@color
+M=-1 // set color = black
+@LOOP
+0;JMP
+
+(WHITE)
+@color
+M=0  //set color = 0
+
+(LOOP)
+@i
+D=M
+@iter
+D=D-M
+@INPUTLOOP
+D;JEQ  // case i=n (finished coloring the screen)
+
+@i
+D=M
+@address  // address += i
+M=M+D
+
+@color
+D=M
+@address
+A=M // color 16 bit
+M=D
+
+@i
+M=M+1  //i += 1
+
+@LOOP
+0;JMP
