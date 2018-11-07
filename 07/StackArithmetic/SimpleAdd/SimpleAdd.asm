3b314d4cea089bd75b86565d5956fff94e763f22
zohar bouchnik <zohar.bouchnik@mail.huji.ac.il>
2018-11-07 15:45:21 +0200

    started ex 7 - got to static pop

---
+++ 07/StackArithmetic/SimpleAdd/SimpleAdd.asm
@@ -0,0 +1,17 @@
+// push constant 7
+@7
+D=A
+@sp
+A=M
+M=D
+@sp
+M=M+1
+// push constant 8
+@8
+D=A
+@sp
+A=M
+M=D
+@sp
+M=M+1
+// add
---
+++ 07/testing.asm
@@ -0,0 +1,125 @@
+// push constant 7
+@7
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 3
+@3
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 2
+@2
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 4
+@4
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop local 1
+@1
+D=A
+@LCL
+D=M+D
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+A=M
+M=D
+// pop local 2
+@2
+D=A
+@LCL
+D=M+D
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+A=M
+M=D
+// pop local 3
+@3
+D=A
+@LCL
+D=M+D
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+A=M
+M=D
+// push local 1
+@1
+D=A
+@LCL
+D=M+D
+A=D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 100
+@100
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 100
+@100
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 100
+@100
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 99
+@99
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop static 1
+@SP
+M=M-1
+A=M
+D=M
+@testing.1
+M=D
