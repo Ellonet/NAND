b7daad5b8931fad951a97db00abd4ba71ca46d5a
zohar bouchnik <zohar.bouchnik@mail.huji.ac.il>
2018-11-17 11:50:27 +0200

    done done done 07

--- 07/ex07/Main.py
+++ 07/ex07/Main.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
 			code_writer = CodeWriter(os.path.abspath(file), False, counter)
 			final_file.extend(code_writer.get_all_commands())
 			counter = code_writer.label_counter
-		# print(os.path.basename(user_input))
 		with open(os.path.basename(user_input) + ".asm", "w") as file:
 			# file.writelines(final_file)
 			for asm in final_file:
