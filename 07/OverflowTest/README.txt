1bbe54e2345b15ea5dafb103a56dc1917864f9c6
zohar bouchnik <zohar.bouchnik@mail.huji.ac.il>
2018-12-12 15:37:30 +0200

    ex 10 - all the functions except Expressions

--- 07/OverflowTest/README.txt
+++
@@ -1,5 +0,0 @@
-Tests Created by Evyatar.
-Tests for nand2tetris, project 7.
-
-'AllTogether' contains all other tests.
-The small tests are here to make debugging  easier.
--- 10/project10/Main.py
+++
@@ -1,8 +0,0 @@
-from CompilationEngine import CompilationEngine
-import sys
-
-if __name__ == '__main__':
-    input_file = sys.argv[1]
-    output_file = input_file.replace(".jack", ".xml")
-    compilation_engine = CompilationEngine(input_file, output_file)
-    print(compilation_engine.to_output_file)
