cf537560d3e921635b62a40c7f736cf2f1c74d0e
zohar bouchnik <zohar.bouchnik@mail.huji.ac.il>
2018-12-17 15:32:46 +0200

    ex 11:

--- 11/project11/SymbolTable.py
+++ 11/project11/SymbolTable.py
@@ -2,7 +2,12 @@ class SymbolTable:
     def __init__(self):
         """
         Creates a new empty symbol table.
+        we use two separate hash tables to implement the symbol table: one for the class-scope
+        and another one for the subroutine-scope. When a new subroutine is started,
+        the subroutine-scope table should be cleared.
         """
+        self.class_symbol_table = {}
+        self.subroutine_symbol_table = {}
 
     def start_subroutine(self):
         """
@@ -30,4 +35,27 @@ class SymbolTable:
         """
         return
 
-    def 
+    def kind_of(self, name):
+        """
+        Returns the kind of the named identifier in the current scope. Returns NONE if the
+        identifier is unknown in the current scope.
+        :param name:
+        :return: STATIC, FIELD, ARG, VAR, NONE
+        """
+        return
+
+    def type_of(self, name):
+        """
+        Returns the type of the named identifier in the current scope.
+        :param name:
+        :return:
+        """
+        return
+
+    def index_of(self, name):
+        """
+        Returns the index assigned to named identifier.
+        :param name:
+        :return:
+        """
+        return
